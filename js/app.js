5b40ebcfcb555201737711e7663291572736d2b9
skyrailthe <skyrail1994@gmail.com>
2020-09-13 11:31:28 +0300

    добавил фандом покемоны, небольшие правки дизайна

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@
             new Route('home', 'home.html', true),
             new Route('klinok', 'klinok.html'),
             new Route('bokunohero', 'bokunohero.html'),
-            new Route('sonicchar', 'sonicchar.html')
+            new Route('sonicchar', 'sonicchar.html'),
+            new Route('pokemon', 'pokemon.html')
+
 
 
         ]);
